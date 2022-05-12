ca060558a8e8c4cde7535e929960f487ff73f7e6
Amin#0506 <95435963+aminmarh@users.noreply.github.com>
2022-05-12 22:51:14 +0200

    azert

--- Demineur_IBOOUDA_MARHERAOUI_111_102/Form1.Designer.vb
+++ Demineur_IBOOUDA_MARHERAOUI_111_102/Form1.Designer.vb
@@ -87,7 +87,7 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(146, 22)
+        Me.Label1.Location = New System.Drawing.Point(174, 48)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(176, 39)
         Me.Label1.TabIndex = 5
