e240549f98af5560a8fdd456969e83b2f7557783
Amin#0506 <95435963+aminmarh@users.noreply.github.com>
2022-05-19 20:44:48 +0200

    le code du jeu (en cours)

---
+++ Demineur_IBOOUDA_MARHERAOUI_111_102/ModuleJeu.vb
@@ -0,0 +1,19 @@
+﻿Module ModuleJeu
+
+    Private Structure Buton
+        Dim nbMines As Integer
+        Dim estMine As Boolean
+        Dim estDesmasquee As Boolean
+        Dim estMarquee As Boolean
+    End Structure
+
+    Public Sub genererMines()
+        Dim c As Buton
+        c.estMine = True
+        c.estDesmasquee = False
+        c.nbMines = 0
+
+
+    End Sub
+
+End Module
